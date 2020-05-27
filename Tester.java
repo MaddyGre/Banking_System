113ac138bd515a76695824ad02b8e6ce64f0f29a
BlueMoon <45042001+MaddyGre@users.noreply.github.com>
2020-05-27 17:57:03 +0100

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,18 @@
+package com.maddy;
+
+import java.util.Scanner;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+		Menu obj = new Menu("Jonathan", "Witty");		
+		obj.displayMenu();
+		
+//		Scanner scan = new Scanner(System.in);
+//		int option = scan.nextLine().charAt(0);
+		
+
+	}
+
+}
